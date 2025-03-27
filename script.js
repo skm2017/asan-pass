02d8a8aac16d817bf383cd476c9dea90bf324988
Sam Madadi <workmail.skm@gmail.com>
2025-03-27 12:42:34 +0430

    password generator logic updated

--- script.js
+++ script.js
@@ -34,24 +34,45 @@ function copyToClipboard() {
 
 function passwordGenerator(passLen, incCap, incSmall, incNum, incSpecial) {
     let charSet = '';
+    let password = '';
 
-    if (incCap) charSet += CAP_LETTERS;
-    if (incSmall) charSet += SMALL_LETTERS;
-    if (incNum) charSet += NUMBERS;
-    if (incSpecial) charSet += SPECIAL_CHARS;
+    if (incCap) {
+        charSet += CAP_LETTERS;
+        password += CAP_LETTERS[Math.floor(Math.random() * CAP_LETTERS.length)];
+    }
+    if (incSmall) {
+        charSet += SMALL_LETTERS;
+        password += SMALL_LETTERS[Math.floor(Math.random() * SMALL_LETTERS.length)];
+    }
+    if (incNum) {
+        charSet += NUMBERS;
+        password += NUMBERS[Math.floor(Math.random() * NUMBERS.length)];
+    }
+    if (incSpecial) {
+        charSet += SPECIAL_CHARS;
+        password += SPECIAL_CHARS[Math.floor(Math.random() * SPECIAL_CHARS.length)];
+    }
 
     if (charSet.length === 0) {
         alert("Please select at least one character type!");
         return '';
     }
 
-    return Array.from({ length: passLen }, () => charSet[Math.floor(Math.random() * charSet.length)]).join('');
+    for (let i = password.length; i < passLen; i++) {
+        password += charSet[Math.floor(Math.random() * charSet.length)];
+    }
+
+    return shuffleString(password);
 }
 
 function charList(startChar, endChar) {
     const startCode = startChar.charCodeAt(0);
     const endCode = endChar.charCodeAt(0);
     return String.fromCharCode(...Array.from({ length: endCode - startCode + 1 }, (_, i) => startCode + i));
+}
+
+function shuffleString(str) {
+    return [...str].sort(() => Math.random() - 0.5).join('');
 }
 
 document.querySelector("#generateBtn").addEventListener("click", generatePassword);
